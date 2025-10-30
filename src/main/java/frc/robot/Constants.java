337489b3ecb0d814b71ab03c6dffedee5a675e38
Adam Shafer <153322055+AdamARGS@users.noreply.github.com>
2025-10-30 14:33:02 -0400

    Update Constants.java - angular velocity + acceleration

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -16,7 +16,8 @@ public final class Constants {
     // Driving Parameters - Note that these are not the maximum capable speeds of the robot, 
     // rather the allowed maximum speeds
     public static final double kMaxSpeedMetersPerSecond = 4.8;
-    public static final double kMaxAngularSpeed = 2 * Math.PI; // radians per second
+    public static final double kMaxAngularVelocity = 2 * Math.PI; // radians per second
+    public static final double kMaxAngularAcceleration = Math.PI; // radians per second persecond
 
     // Chassis configuration
     public static final double kTrackWidth = Units.inchesToMeters(26.5); // Distance between centers of right and left wheels on robot
